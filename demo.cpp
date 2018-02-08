212462933b5ee38c011c27fc10264554e08b3654
tssfl <admin@tssfl.com>
2018-02-08 11:29:50 +0200

    Added the C++ Hello World

---
+++ demo.cpp
@@ -0,0 +1,15 @@
+// This is a git usage demo
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+cout<<"Hello World"<<endl;
+
+return 0;
+
+}
